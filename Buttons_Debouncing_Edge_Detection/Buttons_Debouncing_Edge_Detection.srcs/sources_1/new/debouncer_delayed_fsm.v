b9880b2053c5f9902f214ca12e6d0b4324239ff7
raven1911 <vuduclam1911@gmail.com>
2024-12-24 00:11:26 +0700

    setup fsm

--- Buttons_Debouncing_Edge_Detection/Buttons_Debouncing_Edge_Detection.srcs/sources_1/new/debouncer_delayed_fsm.v
+++ Buttons_Debouncing_Edge_Detection/Buttons_Debouncing_Edge_Detection.srcs/sources_1/new/debouncer_delayed_fsm.v
@@ -21,7 +21,56 @@
 
 
 module debouncer_delayed_fsm(
-    input clk,
-    input reset_n
+    input   clk,
+    input   reset_n,
+    input   noisy,
+    input   timer_done,
+    output  timer_reset, debounced
     );
+    
+    reg [1:0]   state_reg,  state_next;
+    parameter   S0 = 2'b00, S1 = 2'b01, S2 = 2'b10, S3 = 2'b11;
+    
+    //Sequential state register
+    always @(posedge clk or negedge reset_n) begin
+        if(~reset_n)    state_reg <= S0;
+        
+        else            state_reg <= state_next;        
+       
+    end
+    
+    //datapath circuit
+    always @(*) begin
+        case (state_reg)
+            S0: begin
+                if(~noisy)  state_next <= S0;
+                else if(noisy) state_next <= S1;
+            end
+            
+            S1: begin
+                if(~noisy) state_next <= S0;
+                else if(noisy & (~timer_done)) state_next <= S1;
+                else if(noisy & timer_done) state_next <= S2;
+            end
+            
+            S2: begin
+                if(noisy) state_next <= S2;
+                else if(~noisy) state_next <= S3;        
+            end
+            
+            S3: begin
+                if(noisy) state_next <= S2;
+                else if((~noisy) & (~timer_done)) state_next <= S3;
+                else if((~noisy) & timer_done) state_next <= S0;
+            end
+            
+            default: state_next <= S0;
+ 
+        endcase 
+    end
+    
+    assign timer_reset = (state_reg == S0) | (state_reg == S2);
+    assign debounced = (state_reg == S2) | (state_reg == S3);
+    
+    
 endmodule
