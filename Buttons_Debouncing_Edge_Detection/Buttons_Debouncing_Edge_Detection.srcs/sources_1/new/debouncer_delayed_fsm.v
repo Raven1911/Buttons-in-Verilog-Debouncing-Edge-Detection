35ea24a1d9ab5a0e7a91d861f228a6bbaea17fb6
raven1911 <vuduclam1911@gmail.com>
2024-12-23 23:42:47 +0700

    setup

---
+++ Buttons_Debouncing_Edge_Detection/Buttons_Debouncing_Edge_Detection.srcs/sources_1/new/debouncer_delayed_fsm.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/23/2024 11:42:14 PM
+// Design Name: 
+// Module Name: debouncer_delayed_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module debouncer_delayed_fsm(
+    input clk,
+    input reset_n
+    );
+endmodule
